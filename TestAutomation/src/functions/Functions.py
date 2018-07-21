3c0fa47d597dce55c14712132fc5227c99df48d9
Alejandro <Alejandro@user-PC>
2018-07-21 15:54:26 -0300

    act 2

--- TestAutomation/src/functions/Functions.py
+++ TestAutomation/src/functions/Functions.py
@@ -12,9 +12,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-
 from src.functions.Inicializar import Inicializar
-
+     
 
 class Functions():
 
