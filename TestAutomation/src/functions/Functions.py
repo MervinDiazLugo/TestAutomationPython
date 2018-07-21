289314b153311aa9204fe915120e544ae1a1ed07
Alejandro <Alejandro@user-PC>
2018-07-21 16:39:00 -0300

    bran43

--- TestAutomation/src/functions/Functions.py
+++ TestAutomation/src/functions/Functions.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from src.functions.Inicializar import Inicializar
-     
+     ####branch temp
 
 class Functions():
 
