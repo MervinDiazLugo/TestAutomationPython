a9b68b2a990184a2f86a36d408700782c124e15a
amine <amine@DESKTOP-L99D640>
2018-07-21 17:59:03 -0300

    Mercadolibre xpath

---
+++ TestAutomation/src/pages/Mercadolibre.py
@@ -0,0 +1,14 @@
+'''
+Created on 21 jul. 2018
+
+@author: amine
+'''
+
+class Mercadolibre:
+    
+    lbl_Categoria_xpath = "html/body/header/div/div[2]/div/ul/li[1]/a"
+    
+    lbl_Tecnologia_xpath = "html/body/header/div/div[2]/div/ul/li[1]/nav/section[1]/ul[1]/li[1]/a"
+    
+    btn_DescargaAplicacion_xpath = "html/body/footer/div[2]/div[2]/a"
+    
