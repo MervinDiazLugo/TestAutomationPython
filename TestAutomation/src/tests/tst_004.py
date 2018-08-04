8eaac515326d86f910b144ed215fa64ea1abec29
amine <amine@DESKTOP-L99D640>
2018-08-04 20:09:12 -0300

    ACTUALIZACION TST_004

--- TestAutomation/src/tests/tst_004.py
+++ TestAutomation/src/tests/tst_004.py
@@ -3,17 +3,35 @@ import unittest
 
 from src.functions.Functions import Functions
 from src.pages.Login import Login
+
+import allure
+import pytest
+
+  
+@allure.feature(u'Login a Trello')
+@allure.story(u'001: probar el acceso de usuario a trello')
+@allure.testcase(u"caso de prueba 00001",u'http://www.trello.com')
+@allure.severity(allure.severity_level.NORMAL)
+@allure.description(u"""conectar a trello con exito:</br>
+  -- escibir la url de trello </br>
+  -- ir a login </br>
+  -- esribir usuario y contraseña </br>
+  -- presionar Enter </br>
+  -- el navegador abre trello y nos muestra la imagen a la derecha </br>""")
   
 class tst_004(unittest.TestCase, Functions):
-
+    
     def setUp(self):
-        self.driver = self.abrir_Navegador()
+        with allure.step(u'conectar a trello con exito'):
+        
+            self.driver = self.abrir_Navegador()
 
     def test_004(self):
-        TEXT = self.driver.find_element_by_xpath(Login.lbl_Titulo_xpath).text
-        print (TEXT)
+        with allure.step(u'escibir la url de trello'):
+            TEXT = self.driver.find_element_by_xpath(Login.lbl_Titulo_xpath).text
+            print (TEXT)
 
-        self.assertEqual(u"Iniciar sesiÃ³n en Trello", TEXT)
+        self.assertEqual(u"Iniciar sesión en Trello", TEXT)
     
     def tearDown(self):
         self.driver.quit()
