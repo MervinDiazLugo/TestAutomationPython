bea9ba738eb43a8a5aa3af9995729f6b8e4ed56b
Alejandro <Alejandro@user-PC>
2018-09-06 19:07:47 -0300

    ejercicios

--- TestAutomation/src/tests/tst_99.py
+++ TestAutomation/src/tests/tst_99.py
@@ -6,28 +6,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException 
-from src.pages.Google import Home
 import time, os, allure
 from builtins import str
 
 
-@allure.feature(u'login Trello')
-@allure.story(u'El usuario se loguea en Trello')
-@allure.testcase(u"Caso de Prueba 00001", u'https://my.tms.org/browse/TESTCASE-39')
+@allure.feature(u'Google')
+@allure.story(u'001: Verificar  RAET in google')
+@allure.testcase(u'Caso de Prueba tst_01')
 @allure.severity(allure.severity_level.NORMAL)
-@allure.description(U"""El Usuario se loguea en Trello y se busca el que se valide el nombre de Usuario :</br>
--- Abrir el navegador. </br>
--- Buscar Trello.</br>""")
-#-- Entrar a la pagina de Inicio de Trello. </br>
-#-- Escribir Usuario y Contraseña. </br>
-#-- Hacer Click en boton de Iniciar Sesion. </br>
-#-- Ir a la etiqueta Login. </br>
-#-- Validar el nombre que debe corresponder.</br>""")
+@allure.description(u"""The PO gives us the following specification:</br>
+El resultado de buqueda sea mayor a 10000  </br>
 
+1- Ingresar a https://www.google.com. </br>
+2- Ingresar RAET el la barra de busqueda y tomar capturas</br>
+3- Verficar los resultados  </br>
+4- Cerrar Navegador </br>
+ </br></br>""")
   
 class tst_01(unittest.TestCase):
 
-    def esperar_elemento(self, XPATH):
+    def esperar_elemento(self, XPATH):          
         try:
             wait = WebDriverWait(self.driver, 10)
             wait.until(EC.visibility_of_element_located((By.XPATH, XPATH)))
@@ -38,6 +36,7 @@ class tst_01(unittest.TestCase):
             print (u"esperar_Xpath: No presente " + XPATH)
             return False
         
+        
     def capturar_Pantalla(self):  
         def hora_Actual():
             hora = time.strftime("%H%M%S")
@@ -45,7 +44,7 @@ class tst_01(unittest.TestCase):
         
         dia = time.strftime("%d-%m-%Y")
         
-        GeneralPath = "C:\\Evidencias\\Python\\"
+        GeneralPath = "C:\\Evidencias\\Python\\" #Directorio de las Capturas#
         DriverTest = "CHROME"
         TestCase = self.__class__.__name__
         horaAct = str(hora_Actual())
@@ -69,32 +68,28 @@ class tst_01(unittest.TestCase):
             self.driver.implicitly_wait(30)
                  
     def test_01(self):
-        with allure.step(u'Paso 2 Buscar "RAET"'):
+        with allure.step(u'Paso 2 Ingresar RAET el la barra de busqueda"'):
             driver = self.driver
             driver.get("https://www.google.com/")
             
+        ################### XPATH A UTLIZAR ############################
         
-            
-            btn_Busque_apath = "//*[@id='tsf']/div[2]/div[3]/center/input[1]"
-            
+            txt_busqueda_xpath  =  "//*[@id='lst-ib']"
             txt_resultado_xpath = "//*[@id='resultStats']"
-            
-            driver.find_element_by_xpath("//*[@id='lst-ib']").send_keys("RAET")
+                      
+            driver.find_element_by_xpath(txt_busqueda_xpath).send_keys("RAET")
             self.capturar_Pantalla()
-            driver.find_element_by_xpath("//*[@id='lst-ib']").send_keys(Keys.ENTER)
+            driver.find_element_by_xpath(txt_busqueda_xpath).send_keys(Keys.ENTER)
             self.esperar_elemento(txt_resultado_xpath)
             self.capturar_Pantalla()
             
         with allure.step(u'PASO 3: Verificar los resultados de busqueda'): 
                
-            Resultados = driver.find_element_by_xpath(txt_resultado_xpath).text
-            print (Resultados)
-            RAET = Resultados.split(" ")[2]
+            Resultado = driver.find_element_by_xpath(txt_resultado_xpath).text
+            print (Resultado)
+            RAET = Resultado.split(" ")[2]
             print ("RAET obtuvo: " + str(RAET) + " Resultados de busqueda")
             
-            if (RAET > 10000):
-                print ("RAET obtuvo mucho")
-    
             
     def tearDown(self):
         with allure.step(u'PASO 4: Cerrar Navegador'):
