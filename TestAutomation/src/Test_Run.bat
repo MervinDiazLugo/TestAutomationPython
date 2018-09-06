38bfb09c0a79643066246fb6c5cd4278a0e1d0e1
Alejandro <Alejandro@user-PC>
2018-09-06 11:46:13 -0300

    ejer

--- TestAutomation/src/Test_Run.bat
+++ TestAutomation/src/Test_Run.bat
@@ -4,6 +4,6 @@ SET PATH=%PATH%;%PYTHONPATH%;
 @echo off 
 echo. ##################### PRUEBAS #####################
 
-C:\Framework\Python36\python.exe -m pytest tst_006.py --alluredir ..\allure-results
+C:\Framework\Python36\python.exe -m pytest tst_99.py --alluredir ..\allure-results
 
 pause
